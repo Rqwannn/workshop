a06cc7f708db83671e4a3bfde6bb83fa50c2fee0
Muhammad Raqwan <raqwan@student.gunadarma.ac.id>
2024-09-12 21:01:06 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 nama_depan = "Muhammad"
 nama_belakang = "Raqwan Kauthar"
-deskripsi = "Saya seorang mahasiswa teknik informatika"
 usia = 20
 
 print(f"Perkenalkan nama saya {nama_depan} {nama_belakang}, Usia saya {usia}")
-
-print(f"{deskripsi}")
