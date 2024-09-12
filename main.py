745bea4bec158c762c84eca0b1e9121a514e40b3
Rqwannn <mohwann1@gmail.com>
2024-09-12 20:48:34 +0700

    menambahkan deskripsi singkat pada program main

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 nama_depan = "Muhammad"
 nama_belakang = "Raqwan Kauthar"
+deskripsi = "Saya seorang mahasiswa teknik informatika"
 usia = 20
 
 print(f"Perkenalkan nama saya {nama_depan} {nama_belakang}, Usia saya {usia}")
+
+print(f"{deskripsi}")
