d3a96f876d245a7723f2fa4897e0b822efddaeaf
Rqwannn <mohwann1@gmail.com>
2024-09-12 21:17:02 +0700

    mengembalikan ke commit update.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 nama_depan = "Muhammad"
-nama_belakang = "Kauthar"
+nama_belakang = "Raqwan Kauthar"
 deskripsi = "Saya seorang mahasiswa teknik informatika"
-usia = 26
+usia = 20
 
 print(f"Perkenalkan nama saya {nama_depan} {nama_belakang}, Usia saya {usia}")
+
+print(f"{deskripsi}")
