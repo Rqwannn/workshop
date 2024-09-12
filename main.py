5f33fb7b275b56082181950470a611740d346160
Rqwannn <mohwann1@gmail.com>
2024-09-12 21:01:48 +0700

    mengubah value dari variabel nama_belakang

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 nama_depan = "Muhammad"
-nama_belakang = "Raqwan Kauthar"
+nama_belakang = "Kauthar"
 deskripsi = "Saya seorang mahasiswa teknik informatika"
-usia = 20
+usia = 26
 
 print(f"Perkenalkan nama saya {nama_depan} {nama_belakang}, Usia saya {usia}")
 
